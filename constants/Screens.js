ebac82ffa0ac187ce317c9a4d82e39030fee4561
fmel025 <00018720@uca.edu.sv>
2024-01-30 23:26:58 -0600

    feat: add screen route constants :sparkles:

--- constants/Screens.js
+++ constants/Screens.js
@@ -2,4 +2,7 @@ export const SCREENS = {
     HOME: "Home",
     ADDPRODUCT: "AddProduct",
     EXPIREDPRODUCTS: "ExpiredProducts",
+    BOTTOMTAB: "BottomTab",
 }
+
+export const TITLE = "Stock Watcher";
