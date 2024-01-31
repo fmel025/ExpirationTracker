9e4bcf89bf9f04842ee98dcc710ec79d3ab85493
fmel025 <00018720@uca.edu.sv>
2024-01-30 23:29:47 -0600

    feat: add bottom tab navigation :sparkles:

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StackNavigation } from './navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
+import { useColorScheme } from 'react-native';
 
 export default function App() {
+
+  const isDarkMode = useColorScheme() === "dark";
+
+  const backgroundStyle = {
+    flex: 1,
+    backgroundColor: isDarkMode ? "#000" : "#fff",
+  }
+
   return (
-    <StackNavigation />
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor='#fff'/>
+      <NavigationContainer>
+        <StackNavigation />
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
--- navigation/index.js
+++ navigation/index.js
@@ -1,51 +1,96 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// Screens
 import HomeScreen from "../screens/Home";
 import AddProductScreen from '../screens/AddProduct';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { SCREENS } from '../constants/Screens';
+
+// Constants
+import { SCREENS, TITLE } from '../constants/Screens';
+
+// Icons
+import { FontAwesome5, Ionicons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export const StackNavigation = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home'>
-                <Stack.Screen
-                    name={SCREENS.HOME}
-                    component={HomeScreen}
-                    options={{
-                        title: "Productos",
-                        headerTitleAlign: "center",
-                    }}
-                />
-                <Stack.Screen
-                    name={SCREENS.ADDPRODUCT}
-                    component={AddProductScreen}
-                    options={{
-                        title: "Añadir un producto",
-                        headerTitleAlign: "center",
-                    }}
-                />
-                <Stack.Screen
-                    name={SCREENS.EXPIREDPRODUCTS}
-                    component={HomeScreen}
-                    options={{
-                        title: "Productos vencidos",
-                        headerTitleAlign: "center",
-                    }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Stack.Navigator initialRouteName={SCREENS.BOTTOMTAB}>
+            <Stack.Screen
+                name={SCREENS.HOME}
+                component={HomeScreen}
+                options={{
+                    title: TITLE,
+                    headerTitleAlign: "center",
+                }}
+            />
+            <Stack.Screen
+                name={SCREENS.ADDPRODUCT}
+                component={AddProductScreen}
+                options={{
+                    title: "Añadir un producto",
+                    headerTitleAlign: "center",
+                }}
+            />
+            <Stack.Screen
+                name={SCREENS.EXPIREDPRODUCTS}
+                component={HomeScreen}
+                options={{
+                    title: "Productos vencidos",
+                    headerTitleAlign: "center",
+                }}
+            />
+            <Stack.Screen
+                name={SCREENS.BOTTOMTAB}
+                component={TabNavigation}
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </Stack.Navigator>
     );
 };
 
-// export const TabNavigation = () => {
-//     return (
-//         <Tab.Navigator>
-//             <Tab.Screen name=''/>
-//         </Tab.Navigator>
-//     );
-// }
+export const TabNavigation = () => {
+    return (
+        <Tab.Navigator screenOptions={{
+            headerShown: false,
+            tabBarStyle: {
+                height: 60,
+                position: 'absolute',
+                bottom: 16,
+                right: 16,
+                left: 16,
+                borderRadius: 10
+            }
+        }}>
+            <Tab.Screen
+                name={SCREENS.HOME}
+                component={HomeScreen}
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ focused }) => (<FontAwesome5 name="home" size={24} color={focused ? 'purple' : 'black'} />),
+                    tabBarShowLabel: false,
+                }}
+            />
+            <Tab.Screen
+                name={SCREENS.ADDPRODUCT}
+                component={AddProductScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => (<Ionicons name="add-circle-sharp" size={40} color={focused ? 'purple' : 'black'} />),
+                    tabBarShowLabel: false,
+                }}
+            />
+            <Tab.Screen
+                name={SCREENS.EXPIREDPRODUCTS}
+                component={AddProductScreen}
+                options={{
+                    tabBarIcon: ({ focused }) => (<FontAwesome5 name="trash" size={25} color={focused ? 'purple' : 'black'} />),
+
+                    tabBarShowLabel: false,
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
